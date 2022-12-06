41dda92da0149b0f57eb80e4358125e746dbd9f8
Chris Vettese <chris.vettese2@gmail.com>
2022-12-05 19:25:26 -0500

    Add project architecture card

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import Head from 'next/head'
 import {Box, Card, CardContent, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+
+  if (!domLoaded) return null;
+
+  document.body.setAttribute('style', 'background: #F0F0F0');
+
   return (
     <div>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <main>
@@ -16,6 +27,7 @@ export default function Home() {
           <Card sx={{width: 'max-content'}}>
             <CardContent>
               <Typography variant='h5'>Project Architecture</Typography>
+              <img src='/assets/architecture.png' alt='Project Architecture'/>
             </CardContent>
           </Card>
         </Box>
