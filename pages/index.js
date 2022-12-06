ed922ef2b3d46c9bef5ed9793665d7a342c9f9de
Chris Vettese <chris.vettese2@gmail.com>
2022-12-05 22:16:46 -0500

    Change title

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 
   return (<div>
     <Head>
-      <title>Create Next App</title>
+      <title>SEG4105 Lab 8</title>
       <link rel="icon" href="/favicon.ico"/>
     </Head>
 
